21278d5ac5569d50c301c5ffefe00cb43ddfbd2e
drsapnapatidar08 <dr.sapnapatidar08@gmail.com>
2026-02-17 16:31:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿const buttons = document.querySelectorAll("a.btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.add("clicked");
+    setTimeout(() => btn.classList.remove("clicked"), 220);
+  });
+});
